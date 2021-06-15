6f3d6cdcf9bb748aaa6641db8c9c322ae34cc4ec
Etienne BENDER <bender.etienne57@gmail.com>
2021-06-15 11:34:30 +0200

    Setup eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
+  extends: [
+    'prettier',
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/typescript/recommended',
@@ -17,13 +18,15 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
-		'@typescript-eslint/explicit-module-boundary-types': 'off',
-		'@typescript-eslint/no-non-null-assertion': 'off',
+    'prettier/prettier': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-		'vue/attributes-order': 'off',
-		'vue/attribute-hyphenation': ['error', 'never']
+    'vue/attributes-order': 'off',
+    'vue/attribute-hyphenation': ['error', 'never']
   },
+  plugins: ['prettier'],
   overrides: [
     {
       files: [
