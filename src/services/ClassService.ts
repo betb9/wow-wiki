90be953acc381b5792204de15df599585836bd42
Etienne BENDER <bender.etienne57@gmail.com>
2021-06-18 16:24:50 +0200

    Add function to load a Class from BNet API

--- src/services/ClassService.ts
+++ src/services/ClassService.ts
@@ -1,5 +1,5 @@
 import { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
-import { ClassesList } from '@/utils/ClassesTypes';
+import { Class, ClassesList } from '@/utils/ClassesTypes';
 
 /**
  * Return all Classes from BNet API
@@ -12,8 +12,21 @@ async function getClassesList(oauthClient: AxiosInstance, parameters?: AxiosRequ
   return response.data;
 }
 
+/**
+ * Return a class from BNet API for given classID
+ * @param oauthClient
+ * @param classId
+ * @param parameters
+ * @return Promise<Class>
+ */
+async function getClass(oauthClient: AxiosInstance, classId: number, parameters?: AxiosRequestConfig): Promise<Class> {
+  const response: AxiosResponse<Class> = await oauthClient.get<Class>(`/data/wow/playable-class/${classId}`, parameters);
+  return response.data;
+}
+
 export function useClassService() {
   return {
-    getClassesList
+    getClassesList,
+    getClass
   };
 }
