7b8d59166eee7ac0a6730b68acef4e3d4fd7b6a5
Etienne BENDER <bender.etienne57@gmail.com>
2021-06-18 14:50:00 +0200

    Add API Class interface

--- src/utils/ClassesTypes.ts
+++ src/utils/ClassesTypes.ts
@@ -4,3 +4,14 @@ export interface ClassesList {
   _links: BNetSelf;
   classes: Array<BNetRef>;
 }
+
+export interface Class {
+  _links: BNetSelf;
+  id: number;
+  name: string;
+  gender_name: Record<string, string>;
+  power_type: BNetRef;
+  specializations: Array<BNetRef>;
+  media: BNetRef;
+  pvp_talent_slots: BNetLink;
+}
