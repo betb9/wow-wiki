4e5c385ffcff0a6fac986cd5a6e310806c64a3cd
Etienne BENDER <bender.etienne57@gmail.com>
2021-06-22 23:36:25 +0200

    Add router guard to add locale from BNetLocale as base route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,16 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHistory, RouteLocationNormalized, RouteRecordRaw } from 'vue-router';
+import { BNetLocale } from '@/utils/types';
 import Home from '../views/Home.vue';
+
+let availableLocalesRegex = '';
+for (const locale of Object.values(BNetLocale)) {
+  availableLocalesRegex += `${locale}|`;
+}
+const baseUrl = `${process.env.BASE_URL}:locale(${availableLocalesRegex})?` || `/:locale(${availableLocalesRegex})?`;
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
+    path: baseUrl,
     name: 'Home',
     component: Home
   }
@@ -13,5 +20,27 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 });
+
+const defaultLocale = BNetLocale.EnglishGreatBritain;
+router.beforeEach((to, from, next) => {
+  const { locale, toUpdate } = checkLocale(to, from);
+  if (toUpdate) next({ params: { locale } });
+  else next();
+});
+
+function checkLocale(to: RouteLocationNormalized, from: RouteLocationNormalized): { toUpdate: boolean; locale: string } {
+  const toLocale = to.params.locale;
+  const fromLocale = from.params.locale;
+  if (toLocale === fromLocale && typeof toLocale === 'string' && toLocale.length)
+    return { toUpdate: false, locale: formatLocale(toLocale) };
+  if (typeof toLocale === 'string' && toLocale.length) return { toUpdate: false, locale: formatLocale(toLocale) };
+  if (typeof fromLocale === 'string' && fromLocale.length) return { toUpdate: true, locale: formatLocale(fromLocale) };
+  return { toUpdate: true, locale: defaultLocale };
+}
+
+function formatLocale(locale: string): string {
+  if (locale.length) return `${locale.slice(0, 2).toLowerCase()}_${locale.slice(3).toUpperCase()}`;
+  else return defaultLocale as string;
+}
 
 export default router;
